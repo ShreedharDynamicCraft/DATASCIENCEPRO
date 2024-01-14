0292af4d8da6ca70988c4ca0dc1a9c0d4664535e
ShreedharDynamicCraft <shreedharanandji@gmail.com>
2024-01-14 11:57:06 +0530

    new update

---
+++ DATA SCIENCE/PYTHON BASIC/hello.py
@@ -0,0 +1,33 @@
+print("hello world")
+
+
+x=5
+print(x)
+y="hello"
+# print(x+y)
+
+h=int(1)
+k=int("55")
+print(k)
+
+txt = "The best things in life are free!"
+print("free" in txt)
+
+b = "Hello, World!"
+print(b[-5:-2]) #if not negative then index will start from index
+
+# Example
+
+# The lower() method returns the string in lower case:
+a = "Hello, World!"
+print(a.lower()) #a.upper
+#to remove white space strip()
+#replace() for to replace char
+#split() for spliting in substring
+
+
+age=5
+txt= "i am {}"
+print(txt.format(age))
+
+
